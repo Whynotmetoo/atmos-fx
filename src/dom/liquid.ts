300aa1e2d2d5b8ffdcad52fc45c723570cedacd0
Whynotmetoo <nicksoncn@foxmail.com>
2026-06-13 13:07:49 -0400

    fix(liquid): adjust dripping liquid transparency factor to 0.25

--- src/dom/liquid.ts
+++ src/dom/liquid.ts
@@ -258,8 +258,8 @@ export function createLiquidDripsController(
 
       svg.style.display = 'block'
       const parsedColor = parseColorRGB(options.color)
-      // Make liquid color slightly semi-transparent, around 30% transparent (70% opacity)
-      svg.style.opacity = String(parsedColor.alpha * 0.7)
+      // Make liquid color slightly semi-transparent
+      svg.style.opacity = String(parsedColor.alpha * 0.25)
 
       // Sync count
       while (drips.length < targets.length) {
